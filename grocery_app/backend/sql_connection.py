101e45a5e0bc2dbff1177923e4d78ed9cc91526f
Sushmayadav908509 <ysushma2020@gmail.com>
2023-12-10 10:46:34 +0530

    new change

--- grocery_app/backend/sql_connection.py
+++ grocery_app/backend/sql_connection.py
@@ -1,11 +1,16 @@
+import datetime
 import mysql.connector
 
-__cnx=None
+__cnx = None
 
 def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='Sushma@123',
-                              host='127.0.0.1',
-                              database='gs')
-    return __cnx
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='Sushma@123', database='gs')
+
+  return __cnx
+    
+  
+    
